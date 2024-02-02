a0f7ba0383e88b551359956deadce01e37ed5937
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-02-02 18:36:59 +0100

    correct png into jpg

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,17 @@ const frameMetadata = getFrameMetadata({
       label: "Begin",
     },
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmR5nbUdQN5H1s28Tuuk62KAeFfD6eq5sSiXbR6thqUg7q/0.png`,
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmR5nbUdQN5H1s28Tuuk62KAeFfD6eq5sSiXbR6thqUg7q/0.jpg`,
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
 export const metadata: Metadata = {
-  title: "A slightly different Cosmic Cowboy",
+  title: "A slightly different Cosmic Cowboys",
   description: "Life is but a dream...",
   openGraph: {
-    title: "A slightly different Cosmic Cowboy",
+    title: "A slightly different Cosmic Cowboys",
     description: "Life is but a dream...",
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmR5nbUdQN5H1s28Tuuk62KAeFfD6eq5sSiXbR6thqUg7q/0.png`],
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmR5nbUdQN5H1s28Tuuk62KAeFfD6eq5sSiXbR6thqUg7q/0.jpg`],
   },
   other: {
     ...frameMetadata,
