dcd2ce2e35212ecb561827469ffcc1ee13aee951
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-02-03 13:44:09 +0100

    added path

--- app/spacecowboys/page.tsx
+++ app/spacecowboys/page.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function RedirectPage() {
-  /*const router = useRouter();
+  const router = useRouter();
 
   useEffect(() => {
     const youtubeUrl = "https://www.youtube.com/watch?v=Ru_H5PiyfSA";
@@ -12,7 +12,7 @@ export default function RedirectPage() {
     window.location.href = youtubeUrl; // For a full page reload redirect
     // Or use Next.js router for client-side redirect (comment out the line above if using this)
     // router.push(youtubeUrl);
-  }, [router]);*/
+  }, [router]);
 
   return (
     <div>
