866a0048751fe399aaf20c64c0ec6d9e5d7dc8bd
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-02-03 13:34:47 +0100

    returned full headers

--- app/api/end/route.ts
+++ app/api/end/route.ts
@@ -12,15 +12,13 @@ export async function POST(req: NextRequest): Promise<Response> {
     path = "";
   }
 
-  return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/${path}`, { status: 302 });
-
-  /*const headers = new Headers();
+  const headers = new Headers();
   headers.set("Location", `${process.env.NEXT_PUBLIC_BASE_URL}/`);
   const response = NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/${path}`, {
     headers: headers,
     status: 302,
   });
-  return response;*/
+  return response;
 }
 
 export const dynamic = "force-dynamic";
