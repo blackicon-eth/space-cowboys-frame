0a3acb695a2bbdc75c5757b1d578ba8153f3f468
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-02-03 14:14:47 +0100

    back to next.config.mjs 2

--- app/api/end/route.ts
+++ app/api/end/route.ts
@@ -14,11 +14,11 @@ export async function POST(req: NextRequest): Promise<Response> {
 
   const headers = new Headers();
   headers.set("Location", `${process.env.NEXT_PUBLIC_BASE_URL}/`);
-  const response = NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/${path}`, {
+
+  return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/${path}`, {
     headers: headers,
     status: 302,
   });
-  return response;
 }
 
 export const dynamic = "force-dynamic";
