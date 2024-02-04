a192ef7b408db2d81fca5024b1c380bffd1496aa
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-02-04 13:54:10 +0100

    testing indian code

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/spacecowboys",
-        destination: "https://www.youtube.com/watch?v=Ru_H5PiyfSA",
-        permanent: false,
-      },
-    ];
-  },
+  async redirects() {},
 };
 
 export default nextConfig;
